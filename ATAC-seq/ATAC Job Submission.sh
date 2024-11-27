645e10f22267d9f07e8b2cf4141003a778f0146d
Souren Haghbin <147211303+S24K@users.noreply.github.com>
2024-11-27 18:10:54 -0500

    Update ATAC Job Submission.sh

--- ATAC-seq/ATAC Job Submission.sh
+++ ATAC-seq/ATAC Job Submission.sh
@@ -7,14 +7,14 @@
 #SBATCH --mem=15g               # Memory required
 #SBATCH -e ATAC_JOB-2.err       # File to write errors to
 #SBATCH --mail-type=END,FAIL    # Notify when the job ends or fails (optional)
-#SBATCH --mail-user=souren.haghbin@uhn.ca  # Email to notify (optional)
+#SBATCH --mail-user=firstnamelastname@gmail.com  # Email to notify (optional)
 
 #Access Folder
-cd /cluster/projects/epigenomics/Souren/fast_q/POP_170
+cd /path/to/folder
 
 #Load the ENCODE pipeline.
 module load atac-seq-pipeline/2.2.2
 
-caper hpc submit $pipeline_home/atac.wdl -i /cluster/home/t134491uhn/job_submission/input_1.json --leader-job-name ATAC_Job --conda --womtool $pipeline_home/womtool-86.jar --cromwell \
+caper hpc submit $pipeline_home/atac.wdl -i /location/to/.json --leader-job-name ATAC_Job --conda --womtool $pipeline_home/womtool-86.jar --cromwell \
 $pipeline_home/cromwell-86.jar --local-loc-dir ./ --backend slurm \
 --slurm-account epigenomics --slurm-partition himem
