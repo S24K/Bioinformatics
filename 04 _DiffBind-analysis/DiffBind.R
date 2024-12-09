1d37a1b755c6b33f2d7ef78f480e5396eaa8337e
Souren Haghbin <147211303+S24K@users.noreply.github.com>
2024-12-09 14:21:24 -0500

    Update DiffBind.R

--- 04 _DiffBind-analysis/DiffBind.R
+++ 04 _DiffBind-analysis/DiffBind.R
@@ -7,41 +7,58 @@ samples <- read.csv("/cluster/home/t134491uhn/DiffBind/samples1.csv", header=TRU
 dbaObj <- dba(sampleSheet=samples)
 
 print("Dba count:")
+
 # Count reads in the specified peaks
 dbaObj <- dba.count(dbaObj, bUseSummarizeOverlaps=TRUE)
 
-print("Saving")
-# Save
-dba.save(dbObj, file=‘Diffbind_Trial1, dir='/cluster/projects/epigenomics/Souren', pre='dba_', ext='RData',
-         bRemoveAnalysis=FALSE, bRemoveBackground=FALSE,
-         bCompress=FALSE)
+# print("Saving dba object...")
+
+# Save the dba object
+# dba.save(
+#  dbaObj,
+#  file="Diffbind_Trial1",
+#  dir="/cluster/projects/epigenomics/Souren",
+#  pre="dba_",
+#  ext="RData",
+#  bRemoveAnalysis=FALSE,
+#  bRemoveBackground=FALSE,
+#  bCompress=TRUE
+# )
 
 print("Dba contrast:")
+
 # Define contrast for analysis (primary vs metastasis)
 dbaObj <- dba.contrast(dbaObj, categories=DBA_CONDITION, minMembers=2)
 
-print("Dba analyze:")
-# Perform differential binding analysis using DESeq2
+print("Performing differential binding analysis...")
+
+# Perform differential binding analysis using DESeq2 and EdgeR
 dbaObj <- dba.analyze(dbaObj, method=DBA_ALL_METHODS)
 
+# Open graphics device to save plots
 png("/cluster/home/t134491uhn/DiffBind/diffbind_plots_1.png")
 
 # Generate correlation heatmap
+print("Plotting heatmap...")
 dba.plotHeatmap(dbaObj, contrast=1, correlations=TRUE)
 
 # PCA plot to assess clustering
+print("Plotting PCA...")
 dba.plotPCA(dbaObj, contrast=1, label=DBA_ID)
 
 # MA plot for differential binding results
+print("Plotting MA plot...")
 dba.plotMA(dbaObj, method=DBA_DESEQ2)
 
 # Retrieve and save results for inspection
+print("Saving differential binding results...")
 dbResults <- dba.report(dbaObj, method=DBA_DESEQ2)
-write.csv(as.data.frame(dbResults), "DiffBind_DESeq2_results.csv")
+write.csv(
+  dbResults,
+  file="/cluster/home/t134491uhn/DiffBind/DiffBind_DESeq2_results.csv"
+)
 
-# Optional Venn diagram (if comparing more than one condition)
-# dba.plotVenn(dbaObj, contrast=1:2)
+# Close graphics device
+dev.off()
 
 print("DiffBind analysis completed successfully.")
-
-dev.off()
